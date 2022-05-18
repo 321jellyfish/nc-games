b997b941f8884e9e0cd6953dae7c7fc98bfd7123
321jellyfish <csljeffreys@gmail.com>
2022-05-18 14:48:24 +0100

    set up error handling in seperate file

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const {
   postComment,
 } = require("./controllers/reviews.controller");
 const { getUsers } = require("./controllers/users.controller");
+const {
+  handlePSQLErrors,
+  handleCustomErrors,
+  handleInternalServerError,
+} = require("./error-handlers");
 const app = express();
 
 app.use(express.json());
@@ -24,17 +29,8 @@ app.use("/*", (request, response, next) => {
   response.status(404).send({ msg: "Not Found" });
 });
 
-app.use((error, request, response, next) => {
-  // console.log(error);
-  if (error.code === "22P02" || error.code === "23502") {
-    response.status(400).send({ msg: "Bad Request" });
-  } else if (error.code === "23503") {
-    response.status(404).send({ msg: "User Not Found" });
-  } else if (response.status) {
-    response.status(error.status).send({ msg: error.msg });
-  } else {
-    response.status(500).send({ msg: "Internal Server Error" });
-  }
-});
+app.use(handlePSQLErrors);
+app.use(handleCustomErrors);
+app.use(handleInternalServerError);
 
 module.exports = app;
